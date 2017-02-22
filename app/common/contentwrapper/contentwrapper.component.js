ca81a2d4c3c9617f30fb21b2046bfe73289af7da
Cristian Ovando <fabian222_90@hotmail.com>
2017-02-22 18:16:19 -0300

    primeros toques de adminlte

---
+++ app/common/contentwrapper/contentwrapper.component.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// /app/common/contentwrapper/contentwrapper.component.js
+export const ContentWrapperComponent = {  
+  template: `
+    <div class="content-wrapper" style="min-height: 916px;">
+      <section class="content-header">
+        <h1>
+          Dashboard
+          <small>Version 2.0</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Dashboard</li>
+        </ol>
+      </section>
+      <section class="content">
+        <div ui-view></div>    
+      </section>
+    </div>
+  `
+}
