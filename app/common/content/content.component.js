600e14bf011be7d9e3a45ac26419290fe7d6c176
Cristian Ovando <fabian222_90@hotmail.com>
2017-03-01 10:13:31 -0300

    corrigiendo merge

--- app/common/content/content.component.js
+++ app/common/content/content.component.js
@@ -1,25 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-// /app/common/contentwrapper/contentwrapper.component.js
-export const ContentComponent = {  
-  template: `
-    <div class="content-wrapper" style="min-height: 916px;">
-      <section class="content-header">
-        <h1>
-          Dashboard
-          <small>Version 2.0</small>
-        </h1>
-        <ol class="breadcrumb">
-          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-          <li class="active">Dashboard</li>
-        </ol>
-      </section>
-      <section class="content">
-        <div ui-view></div>    
-      </section>
-    </div>
-=======
 export const ContentComponent = {  
   template: `
     <div class="content-wrapper" style="min-height: 916px;">
@@ -37,6 +17,5 @@ export const ContentComponent = {
       <div ui-view></div>    
     </section>
   </div>
->>>>>>> 11101aef433ed96f70b79d6dd13641be713bbfad
   `
 };
--- app/common/header/header.module.js
+++ app/common/header/header.module.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-=======
 'use strict';
 
->>>>>>> 11101aef433ed96f70b79d6dd13641be713bbfad
 import angular from 'angular';
-
 import { HeaderComponent } from './header.component';
 
-<<<<<<< HEAD
-export const HeaderModule = angular
-=======
 export const HeaderModule = angular  
->>>>>>> 11101aef433ed96f70b79d6dd13641be713bbfad
   .module('header', [])
   .component('header', HeaderComponent)
   .name;
